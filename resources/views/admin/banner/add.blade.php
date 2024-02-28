6a57d7d043401be05f667f73cee609d889b67b18
cntya <chintyamaharani699@gmail.com>
2024-02-28 14:35:43 +0700

    upload 14

--- resources/views/admin/banner/add.blade.php
+++ resources/views/admin/banner/add.blade.php
@@ -1,38 +1,32 @@
-{{-- <div class="row"> --}}
-    {{-- <div class="col-md-6"> --}}
-        <div class="card">
-            <div class="card-body">
+<div class="card">
+    <div class="card-body">
+        {{-- CKEditor --}}
+        <script src="https://cdn.ckeditor.com/4.18.0/standard/ckeditor.js"></script>
+        <form action="" method="POST" enctype="multipart/form-data">
+            @csrf  
 
-            {{--CKEditor--}}
-                <script src="https://cdn.ckeditor.com/4.18.0/standard/ckeditor.js"></script>
-                <form action="" method="POST" enctype="multipart/form">
-                    @csrf
-
-
-                    <div class="form-group">
-                        <label for=""><b>headline</b></label>
-                        <input type="text" name="headline"
-                            class="form-control @error('headline') is-invalid @enderror" placeholder="headline">
-                    </div>
-
-                    <div class="form-group">
-                        <label for=""><b>Deskripsi</b></label>
-                        <textarea name="desc" id="content" cols="30" rows="10"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for=""><b>Gambar </b></label>
-                        <input type="file" name="gambar" class="" placeholder="gambar">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary"> Simpan</button>
-
-                </form>
+            <div class="form-group">
+                <label for="headline"><b>Headline</b></label>
+                <input type="text" name="headline" id="headline" class="form-control @error('headline') is-invalid @enderror" placeholder="Headline">
             </div>
-        </div>
 
+            <div class="form-group">
+                <label for="desc"><b>Deskripsi</b></label>
+                <textarea name="desc" id="desc" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="gambar"><b>Gambar</b></label>
+                <input type="file" name="gambar" id="gambar" class="form-control-file" placeholder="Gambar">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
     </div>
 </div>
+
 <script>
-    CKEDITOR.replace('content');
+    CKEDITOR.replace('desc', {
+        autoParagraph: false
+    });
 </script>
